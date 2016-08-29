9f0e1d755a5a8741a5e3376b2aad794042ac72db
Basit Munir <basitmunir.pk@gmail.com>
2016-08-29 14:11:51 +0500

    services implementation, child components, use of components within components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ProductListComponent} from './products/product-list.component';
+import { ProductService} from './products/product.service';
+
 @Component({
     selector: 'my-app',
     template: `<h1>{{pageTitle}}</h1>
     			<div>
     				<pm-products></pm-products>
     			</div>`,
-   	directives: [ProductListComponent]
+   	directives: [ProductListComponent],
+   	providers: [ProductService]
 })
 export class AppComponent {
 	pageTitle: string= "Product Management";
